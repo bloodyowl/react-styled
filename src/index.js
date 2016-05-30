43c1fdd668588ef306a282bb241c335ff60a3723
José Moreira <jose.moreira@findhit.com>
2016-05-30 12:55:51 +0100

    Use a counter to avoid unintended use/unuse calls
    Edited on GitHub, didn't tested, sorry.

--- src/index.js
+++ src/index.js
@@ -13,22 +13,35 @@ export default (styles) => {
       StyledComponent.prototype.componentWillMount
     const componentWillUnmount =
       StyledComponent.prototype.componentWillUnmount
-
+    
+    let mountCount = 0
+    
     Object.assign(
       StyledComponent.prototype,
       {
         componentWillMount() {
-          styles.use()
+          mountCount++
+
+          if ( mountCount === 1 ) {
+            styles.use()
+          }
+
           if(componentWillMount) {
             componentWillMount.call(this)
           }
         },
 
         componentWillUnmount() {
+          mountCount--
+
           if(componentWillUnmount) {
             componentWillUnmount.call(this)
           }
-          styles.unuse()
+          
+          if ( mountCount === 0 ) {
+            styles.unuse()
+          }
+
         },
       }
     )
